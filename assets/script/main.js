8f6758d97e52e4f2ea91fe712758022c318fe581
Maxim Leyzerovich <service@duqe.com>
2015-02-07 00:52:46 -0500

    Removing head.js

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,3 +1,26 @@
 $(document).ready(function() {
-	$('body').append(' + Scripts Loaded');
+
+	// hypher();
+
+	$('img').unveil(200, function() {
+	  $(this).load(function() {
+	    console.log(this.src + ' loaded');
+	  });
+	});
+
 });
+
+// function hypher() {
+//	head.load('assets/script/lib/hypher.js', function() {
+//		$('body').hyphenate('en-us');
+//		console.log('Hypher Loaded');
+//	});
+// }
+
+function imagesFade() {
+	$('img').addClass('image-loading');
+	$('img').load(function() {
+		$(this).removeClass('image-loading');
+		$(this).addClass('image-loaded');
+	});
+}
